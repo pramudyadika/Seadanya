ddd8542214e85dae3a2676626b9d42e536ab52ce
Pramudya Kusuma H <93606503+pramudyadika@users.noreply.github.com>
2022-11-08 23:02:24 +0700

    connect to postgre

--- Seadanya_testing/Seadanya_testing/Program.cs
+++ Seadanya_testing/Seadanya_testing/Program.cs
@@ -16,7 +16,7 @@ namespace Seadanya_testing
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new SignUp());
         }
     }
 }
